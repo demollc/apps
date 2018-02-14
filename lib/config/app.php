58338b4f98f09bbf813050c970d4778d17610370
Alexkurd <support@demollc.pw>
2018-02-14 11:59:09 +0000

    1.1

--- lib/config/app.php
+++ lib/config/app.php
@@ -8,7 +8,7 @@ return array (
     24 => 'img/apps24.png',
     16 => 'img/apps16.png',
   ),
-  'version' => '1.0',
+  'version' => '1.1',
   'vendor' => '991739',
   'plugins' => true,
   'frontend' => true,
